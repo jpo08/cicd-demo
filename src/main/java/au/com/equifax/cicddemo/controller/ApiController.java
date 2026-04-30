a779a275034059ca817f88083c101c93d43e4b92
jpo08 <juanpabloordonezjimenez@gmail.com>
2026-04-30 01:54:11 -0500

    html

--- src/main/java/au/com/equifax/cicddemo/controller/ApiController.java
+++ src/main/java/au/com/equifax/cicddemo/controller/ApiController.java
@@ -1,8 +1,6 @@
 package au.com.equifax.cicddemo.controller;
 
 import au.com.equifax.cicddemo.domain.EnvDetail;
-
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,8 +10,13 @@ import java.net.UnknownHostException;
 @RestController
 public class ApiController {
 
-    @GetMapping("/")
-    public String home() {
-        return "Hola desde CI/CD Demo v2 - Pipeline funcionando!";
+    @RequestMapping("/")
+    public EnvDetail home() throws UnknownHostException {
+        EnvDetail env=new EnvDetail();
+        InetAddress inetAddress = InetAddress.getLocalHost();
+        env.setHostname(inetAddress.getHostName());
+        env.setIp(inetAddress.getHostAddress());
+        env.setOs(System.getProperty("os.name"));
+        return env;
     }
 }
